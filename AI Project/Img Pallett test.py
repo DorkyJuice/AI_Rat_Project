799623d0db365bf2e4c72bb098d04d022bfc54fa
Jessika Broder <98977778+DorkyJuice@users.noreply.github.com>
2024-03-26 10:25:11 -0700

    added canny

--- AI Project/Img Pallett test.py
+++ AI Project/Img Pallett test.py
@@ -35,25 +35,25 @@ def colorRat(color_count):
                     new_color = color_count[random.randint(0, len(color_count)-1)][1]
                     img2.putpixel( (x,y), (new_color[0], new_color[1], new_color[2]))
 
-def calcFitness():
-    #save the crop and recolored rat
-    img1crop.save(r"C:\Users\jessb\Documents\AI_Rat_Project\AI Project\Temp\crop.jpg")
-    img2.save(r"C:\Users\jessb\Documents\AI_Rat_Project\AI Project\Temp\coloredRat.png")
-    #open them in cv2 (might change this down the line if it takes to long)
-    image1_rgb = cv2.imread(r"C:\Users\jessb\Documents\AI_Rat_Project\AI Project\Temp\crop.jpg")
-    image2_rgb = cv2.imread(r"C:\Users\jessb\Documents\AI_Rat_Project\AI Project\Temp\coloredRat.png")
-    #convert the RGB values to lab
-    image1_lab = cv2.cvtColor(image1_rgb.astype(np.float32) / 255, cv2.COLOR_RGB2Lab)
-    image2_lab = cv2.cvtColor(image2_rgb.astype(np.float32) / 255, cv2.COLOR_RGB2Lab)
-    #get the difference of the lab values between the 2 images
-    delta_E = colour.delta_E(image1_lab, image2_lab)
-    #get the mean of the difference
-    mean = float(np.mean(delta_E))
-    #calculate fitness value of color difference
-    fitness = 100 * (mean**2 - mean)**2 + (1 - mean)**2
+# def calcFitness():
+#     #save the crop and recolored rat
+#     img1crop.save(r"C:\Users\jessb\Documents\AI_Rat_Project\AI Project\Temp\crop.jpg")
+#     img2.save(r"C:\Users\jessb\Documents\AI_Rat_Project\AI Project\Temp\coloredRat.png")
+#     #open them in cv2 (might change this down the line if it takes to long)
+#     image1_rgb = cv2.imread(r"C:\Users\jessb\Documents\AI_Rat_Project\AI Project\Temp\crop.jpg")
+#     image2_rgb = cv2.imread(r"C:\Users\jessb\Documents\AI_Rat_Project\AI Project\Temp\coloredRat.png")
+#     #convert the RGB values to lab
+#     image1_lab = cv2.cvtColor(image1_rgb.astype(np.float32) / 255, cv2.COLOR_RGB2Lab)
+#     image2_lab = cv2.cvtColor(image2_rgb.astype(np.float32) / 255, cv2.COLOR_RGB2Lab)
+#     #get the difference of the lab values between the 2 images
+#     delta_E = colour.delta_E(image1_lab, image2_lab)
+#     #get the mean of the difference
+#     mean = float(np.mean(delta_E))
+#     #calculate fitness value of color difference
+#     fitness = 100 * (mean**2 - mean)**2 + (1 - mean)**2
 
-    print(fitness)
-    return fitness
+    # print(fitness)
+    # return fitness
 
 def pasteImg():
     # Pasting img2 image on top of img1  
@@ -61,9 +61,10 @@ def pasteImg():
     img1.paste(img2, (0,1000), mask = img2) 
 
 def showImg():
+    img1.save(r"C:\Users\jessb\Documents\AI_Rat_Project\AI Project\img1.jpg")
     img1.show()
 
 colorRat(getPalette())
-calcFitness()
+#calcFitness()
 pasteImg()
 showImg()
---
+++ AI Project/canny.py
@@ -0,0 +1,18 @@
+import cv2 
+  
+img = cv2.imread(r"C:\Users\jessb\Documents\AI_Rat_Project\AI Project\img1.jpg")  # Read image 
+cv2.resize(img, (400, 543))
+# Setting parameter values 
+t_lower = 50  # Lower Threshold 
+t_upper = 150  # Upper threshold 
+  
+# Applying the Canny Edge filter 
+edge = cv2.Canny(img, t_lower, t_upper) 
+
+cv2.imshow('original', img) 
+
+cv2.imshow('edge', edge) 
+cv2.resizeWindow('edge', (400,543))
+
+cv2.waitKey(0) 
+cv2.destroyAllWindows() 
