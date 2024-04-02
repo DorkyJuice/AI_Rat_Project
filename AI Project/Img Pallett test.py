0417ef6968399ebd981e1628a20b467b5d070e1c
CasemackP <116687227+CasemackP@users.noreply.github.com>
2024-04-02 16:29:43 -0700

    Added population?

--- AI Project/Img Pallett test.py
+++ AI Project/Img Pallett test.py
@@ -6,44 +6,50 @@ import numpy as np
 import colour
   
 # Opening the primary image (used in background) 
-img1 = Image.open(r"C:\Users\rude3\Documents\GitHub\AI_Rat_Project\AI Project\DistantMountains.jpg") 
+imgBack = Image.open(r"C:\Users\casey\OneDrive\Documents\GitHub\AI_Rat_Project\AI Project\DistantMountains.jpg") 
 
 #crop the background image to the area behind the rat
-img1crop = img1.crop((0, 1000, 164, 1062))
-  
-# Opening the secondary image (overlay image) 
-img2 = Image.open(r"C:\Users\rude3\Documents\GitHub\AI_Rat_Project\AI Project\Rat.png") 
+imgBackcrop = imgBack.crop((0, 1000, 164, 1062))
+rat = []
+for x in 20:
+    # Opening the secondary image (overlay image) 
+    rat[x] = [Image.open(r"C:\Users\casey\OneDrive\Documents\GitHub\AI_Rat_Project\AI Project\Rat.png")]
 
 fitness = 0
 
+def getRat():
+    for x in 20:
+        rat[x] = Image.open(r"C:\Users\casey\OneDrive\Documents\GitHub\AI_Rat_Project\AI Project\Temp\coloredRat" + x + ".png")
+
 def getPalette():
-    reduced = img1.convert("P", palette=Image.Palette.WEB) # convert to web palette (216 colors)
+    reduced = imgBack.convert("P", palette=Image.Palette.WEB) # convert to web palette (216 colors)
     palette = reduced.getpalette() # get palette as [r,g,b,r,g,b,...]
     palette = [palette[3*n:3*n+3] for n in range(256)] # group 3 by 3 = [[r,g,b],[r,g,b],...]
     color_count = [(n, palette[m]) for n,m in reduced.getcolors()]
     color_count.sort(reverse=True) #sort color frequency in descending order
+    print(color_count)
     return color_count
 
-def colorRat(color_count):
+def colorRat(color_count, img):
     # Get the size of the image
-    width = int(img2.width)
-    height = int(img2.height)
+    width = int(img.width)
+    height = int(img.height)
 
     #Process every pixel
     for x in range(width):
         for y in range(height):
-            current_color = img2.getpixel( (x,y) )
+            current_color = img.getpixel( (x,y) )
             if current_color != (0, 0, 0, 0):
                     new_color = color_count[random.randint(0, len(color_count)-1)][1]
-                    img2.putpixel( (x,y), (new_color[0], new_color[1], new_color[2]))
+                    img.putpixel( (x,y), (new_color[0], new_color[1], new_color[2]))
 
-def calcFitness():
+def calcFitness(img):
 #     #save the crop and recolored rat
-     img1crop.save(r"C:\Users\rude3\Documents\GitHub\AI_Rat_Project\AI Project\Temp\crop.jpg")
-     img2.save(r"C:\Users\rude3\Documents\GitHub\AI_Rat_Project\AI Project\Temp\coloredRat.png")
+     imgBackcrop.save(r"C:\Users\casey\OneDrive\Documents\GitHub\AI_Rat_Project\AI Project\Temp\crop.jpg")
+     img.save(r"C:\Users\casey\OneDrive\Documents\GitHub\AI_Rat_Project\AI Project\Temp\coloredRat.png")
 #     #open them in cv2 (might change this down the line if it takes to long)
-     image1_rgb = cv2.imread(r"C:\Users\rude3\Documents\GitHub\AI_Rat_Project\AI Project\Temp\crop.jpg")
-     image2_rgb = cv2.imread(r"C:\Users\rude3\Documents\GitHub\AI_Rat_Project\AI Project\Temp\coloredRat.png")
+     image1_rgb = cv2.imread(r"C:\Users\casey\OneDrive\Documents\GitHub\AI_Rat_Project\AI Project\Temp\crop.jpg")
+     image2_rgb = cv2.imread(r"C:\Users\casey\OneDrive\Documents\GitHub\AI_Rat_Project\AI Project\Temp\coloredRat.png")
 #     #convert the RGB values to lab
      image1_lab = cv2.cvtColor(image1_rgb.astype(np.float32) / 255, cv2.COLOR_RGB2Lab)
      image2_lab = cv2.cvtColor(image2_rgb.astype(np.float32) / 255, cv2.COLOR_RGB2Lab)
@@ -64,26 +70,30 @@ def mutate():
 
 def crossover():
     #crossover the color of the rat
+    
     return
 
 def select():
     #select the best color for the rat
     if(fitness < 0.1):
+        pasteImg()
         showImg()
     return
 
-def pasteImg():
-    # Pasting img2 image on top of img1  
+def pasteImg(img):
+    # Pasting img image on top of imgBack  
     # starting at coordinates (0, 0) 
-    img1.paste(img2, (0,1000), mask = img2) 
+    imgBack.paste(img, (0,1000), mask = img) 
 
 def showImg():
-    img1.save(r"C:\Users\rude3\Documents\GitHub\AI_Rat_Project\AI Project\img1.jpg")
-    img1.show()
+    imgBack.save(r"C:\Users\casey\OneDrive\Documents\GitHub\AI_Rat_Project\AI Project\imgBack.jpg")
+    imgBack.show()
     quit()
 
+colors = getPalette()
 
-colorRat(getPalette())
-calcFitness()
-pasteImg()
-showImg()
+while True:
+    for x in 20:
+        colorRat(colors, rat[x])
+        calcFitness(rat[x])
+    getRat()
